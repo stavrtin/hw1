1de48cf85fd26e20bb73b334876761d58d220078
stavrtin <stavrtin@yandex.ru>
2023-11-26 23:37:38 +0300

    mos_elg_2

--- project_dj_hw/settings.py
+++ project_dj_hw/settings.py
@@ -20,17 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = "django-insecure-18+z@+*i-!_k%#t4_9$fv6on3l-c)twtbu(rfsu0r0*+n1+-#y"
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-18+z@+*i-!_k%#t4_9$fv6on3l-c)twtbu(rfsu0r0*+n1+-#y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+DEBUG = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
-    'stavr.pythonanywhere.com',
+    # 'stavr.pythonanywhere.com',
 
 ]
 
@@ -83,17 +83,17 @@ WSGI_APPLICATION = "project_dj_hw.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db_shop.sqlite3",
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stavr$<your_database_name>',
-        'USER': 'stavr',
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'stavr.mysql.pythonanywhere-services.com',
-        'OPTIONS':    {
-            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode = 'STRICT_TRANS_TABLES'",
-                        'charset': 'utf8mb4',
-            },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db_shop.sqlite3",
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'stavr$<your_database_name>',
+        # 'USER': 'stavr',
+        # 'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        # 'HOST': 'stavr.mysql.pythonanywhere-services.com',
+        # 'OPTIONS':    {
+        #     'init_command': "SET NAMES 'utf8mb4'; SET sql_mode = 'STRICT_TRANS_TABLES'",
+        #                 'charset': 'utf8mb4',
+        #     },
     }
 }
 
